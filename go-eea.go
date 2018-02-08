a09c5b3d34e18e1cd8e713ebff194bd02dc1ee16
jakoblorz <jakob@lorz.org>
2018-02-08 17:40:28 +0100

    extended to package to allow external import

--- go-eea.go
+++ go-eea.go
@@ -4,83 +4,17 @@ import (
 	"fmt"
 
 	"github.com/caarlos0/env"
+	eea "github.com/jakoblorz/go-eea/lib"
 )
-
-type ExtendedEuclidianRow struct {
-	A int
-	B int
-	Q int
-	S int
-	T int
-}
-
-type ExtendedEuclidianParameters struct {
-	A int `env:"A"`
-	B int `env:"B"`
-	S int
-	T int
-}
-
-func (parameters *ExtendedEuclidianParameters) calculate() []*ExtendedEuclidianRow {
-
-	// iterate using ggt-algorithm to detect total iterations required
-	a := parameters.A
-	b := parameters.B
-
-	count := 0
-	for b != 0 {
-
-		t := b
-		b = a % b
-		a = t
-		count++
-	}
-
-	// build simple pointer array for the calculated values
-	values := make([]*ExtendedEuclidianRow, count+1)
-
-	for i := range values {
-
-		if i != 0 {
-
-			t := parameters.B
-			parameters.B = parameters.A % parameters.B
-			values[i-1].Q = (parameters.A - parameters.B) / t
-			parameters.A = t
-
-			if i == count-1 {
-				parameters.S = 1
-			}
-		}
-
-		values[i] = &ExtendedEuclidianRow{
-			A: parameters.A,
-			B: parameters.B,
-			Q: 0,
-			S: parameters.S,
-			T: parameters.T,
-		}
-	}
-
-	// iterate in reverse and skip the first/last one
-	// as s, t are know already there
-	for i := len(values) - 2; i >= 0; i-- {
-		values[i].S = values[i+1].T
-		values[i].T = values[i+1].S - values[i].Q*values[i+1].T
-	}
-
-	return values
-
-}
 
 func main() {
 
-	cfg := ExtendedEuclidianParameters{}
+	cfg := eea.ExtendedEuclidianParameters{}
 	if err := env.Parse(&cfg); err != nil {
 		panic(err)
 	}
 
-	for i, r := range cfg.calculate() {
+	for i, r := range cfg.Calculate() {
 		fmt.Printf("%d: A=%d B=%d Q=%d S=%d T=%d\n", i, r.A, r.B, r.Q, r.S, r.T)
 	}
 }
---
+++ lib/eea.go
@@ -0,0 +1,68 @@
+package eea
+
+type ExtendedEuclidianRow struct {
+	A int
+	B int
+	Q int
+	S int
+	T int
+}
+
+type ExtendedEuclidianParameters struct {
+	A int `env:"A"`
+	B int `env:"B"`
+	S int
+	T int
+}
+
+func (parameters *ExtendedEuclidianParameters) Calculate() []*ExtendedEuclidianRow {
+
+	// iterate using ggt-algorithm to detect total iterations required
+	a := parameters.A
+	b := parameters.B
+
+	count := 0
+	for b != 0 {
+
+		t := b
+		b = a % b
+		a = t
+		count++
+	}
+
+	// build simple pointer array for the calculated values
+	values := make([]*ExtendedEuclidianRow, count+1)
+
+	for i := range values {
+
+		if i != 0 {
+
+			t := parameters.B
+			parameters.B = parameters.A % parameters.B
+			values[i-1].Q = (parameters.A - parameters.B) / t
+			parameters.A = t
+
+			if i == count-1 {
+				parameters.S = 1
+			}
+		}
+
+		values[i] = &ExtendedEuclidianRow{
+			A: parameters.A,
+			B: parameters.B,
+			Q: 0,
+			S: parameters.S,
+			T: parameters.T,
+		}
+	}
+
+	// iterate in reverse and skip the first/last one
+	// as s, t are know already there
+	for i := len(values) - 2; i >= 0; i-- {
+		values[i].S = values[i+1].T
+		values[i].T = values[i+1].S - values[i].Q*values[i+1].T
+	}
+
+	return values
+
+}
